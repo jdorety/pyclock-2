f1fff82cc20090f98bd95bdc6a520bff3f07b2b3
Jonathan Dorety <kyullog@gmail.com>
2019-10-20 22:14:57 -0700

    bedtime and wakeup are now variables

--- chrono.py
+++ chrono.py
@@ -12,10 +12,10 @@ class Chrono:
         self.current_min = self.current_time.tm_min
         self.current_sec = self.current_time.tm_sec
         self.daytime = bool
-        self.bedtime = [7, 0]
+        self.bedtime = [19, 0]
         self.wakeup = [6, 0]
 
-        # TODO: remember to set checks for minutes later one 
+        # TODO: remember to set checks for minutes later on
 
     def tick(self):
         self.current_time = time.localtime()
@@ -29,9 +29,9 @@ class Chrono:
     def check_time(self):
         bt = self.bedtime
         wu = self.wakeup
-        if ( wu[0] < self.current_hour < bt[0]) and self.daytime is not True:
-            self.daytime = not self.daytime
-            sun()
-        elif (wu[0] >= self.current_hour or self.current_hour>= wu[0]) and self.daytime:
-            self.daytime = not self.daytime
-            moon()
+        if (wu[0] <= self.current_hour < bt[0]) and self.daytime is not True:
+            self.daytime = not True  # toggle daytime to True
+            sun()  # display sun on UnicornHat
+        elif (wu[0] > self.current_hour or self.current_hour >= bt[0]) and self.daytime:
+            self.daytime = not False  # set daytime to False
+            moon()  # display moon on UnicornHat
