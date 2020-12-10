6792ea43678f9b60b0cb3f30db3acbc81cc03e13
Jonathan Dorety <jonathan.dorety@gmail.com>
2020-12-09 21:53:16 -0500

    Finally supporting minutes

--- chrono.py
+++ chrono.py
@@ -4,6 +4,17 @@ from moon import moon
 from sun import sun
 
 
+def is_it_day(current_min, current_hour, wake_up_time, bed_time):
+    if current_hour > wake_up_time['hour'] and current_hour < bed_time['hour']:
+        return True
+    elif current_hour == wake_up_time['hour'] and current_min >= wake_up_time['minute']:
+        return True
+    elif current_hour == bed_time['hour'] and current_min < bed_time['minute']:
+        return True
+    else:
+        return False
+
+
 class Chrono:
 
     def __init__(self):
@@ -11,9 +22,14 @@ class Chrono:
         self.current_hour = self.current_time.tm_hour
         self.current_min = self.current_time.tm_min
         self.current_sec = self.current_time.tm_sec
-        self.daytime = bool
-        self.bedtime = [19, 0]
-        self.wakeup = [6, 0]
+        self.bedtime = {'hour': 21, 'minute': 50}
+        self.wakeup = {'hour': 6, 'minute': 0}
+        self.daytime = is_it_day(
+            self.current_min, self.current_hour, self.wakeup, self.bedtime)
+        if self.daytime == True:
+            sun()
+        else:
+            moon()
 
     def tick(self):
         self.current_time = time.localtime()
@@ -21,18 +37,18 @@ class Chrono:
         self.current_min = self.current_time.tm_min
         self.current_sec = self.current_time.tm_sec
         self.check_time()
+        print(self.current_hour, self.current_min)
+        print(self.daytime)
 
         threading.Timer(1, self.tick).start()
 
     def check_time(self):
-        time = [self.current_hour, self.current_min]
         bt = self.bedtime
         wu = self.wakeup
-        # if (wu[0] <= self.current_hour < bt[0]) and (wu[1] <= self.current_min < bt[1]) and self.daytime is not True:
-        if (wu[0] <= time[0] < bt[0]) and self.daytime is not True:
+        daytime = is_it_day(self.current_min, self.current_hour, wu, bt) # check if daytime
+        if daytime == True and self.daytime == False:
             self.daytime = True  # toggle daytime to True
             sun()  # display sun on UnicornHat
-        # elif ((wu[0] > self.current_hour and wu[1] > self.current_min) or (self.current_hour >= bt[0] and self.current_min >= bt[1])) and self.daytime:
-        elif (wu[0] > time[0] or time[0] >= bt[0]) and self.daytime:
+        elif daytime == False and self.daytime == True:
             self.daytime = False  # set daytime to False
             moon()  # display moon on UnicornHat
